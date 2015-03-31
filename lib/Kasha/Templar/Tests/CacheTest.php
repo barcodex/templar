59f1ba8a0f7912a5a2f87a4670291e45fb141b2d
barcodex <codexbar>
2015-03-31 22:13:05 +0200

    more dummy tests, less dependencies, no generator

---
+++ lib/Kasha/Templar/Tests/CacheTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kasha\Templar\Cache;
+
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+	public function testDummy()
+	{
+		$this->assertEquals(1, 1);
+	}
+}
---
+++ lib/Kasha/Templar/Tests/LocatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kasha\Templar\Locator;
+
+class LocatorTest extends PHPUnit_Framework_TestCase
+{
+	public function testDummy()
+	{
+		$this->assertEquals(1, 1);
+	}
+}
