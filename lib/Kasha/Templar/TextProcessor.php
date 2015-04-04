c0624a0be3185b630a969c62b2f0c386cf5685a4
barcodex <codexbar>
2015-04-04 23:13:27 +0200

    optional parameters for doTemplate and loopTemplate

--- lib/Kasha/Templar/TextProcessor.php
+++ lib/Kasha/Templar/TextProcessor.php
@@ -7,17 +7,17 @@ use Temple\Processor;
 class TextProcessor
 	extends Processor
 {
-	public static function doTemplate($moduleName, $templateName, $params)
+	public static function doTemplate($moduleName, $templateName, $params = array())
 	{
 		$templateText = Locator::getInstance()->getTemplate($moduleName, $templateName);
 
 		return Processor::doText($templateText, $params);
 	}
 
-	public static function loopTemplate($moduleName, $templateName, $rows)
+	public static function loopTemplate($moduleName, $templateName, $rows = array())
 	{
 		$templateText = Locator::getInstance()->getTemplate($moduleName, $templateName);
 
-		return Processor::loopText($templateText, $rows);
+		return count($rows) > 0 ? Processor::loopText($templateText, $rows) : '';
 	}
 }
